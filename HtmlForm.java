0f103433ba09923182094501ccc051f7ea494bbb
Sanikaghugare <89474205+Sanikaghugare@users.noreply.github.com>
2021-09-07 21:12:17 +0530

    Add files via upload

---
+++ HtmlForm.java
@@ -0,0 +1,61 @@
+<Doctype Html>
+<html>
+<body style="background-color:yellow;">
+
+
+ 
+<form action="/action_page.php">
+
+
+<fieldset>
+
+
+<legend> Personal information</legend>
+
+
+First Name:<br>
+
+
+<input type="text"Name="first name">
+
+<br>
+Last Name:<br>
+
+
+<input type="text"Name="first name">
+
+<br>
+
+Address:<br>
+
+
+<input type="text"Name="address">
+
+<br>
+
+Date of Birth: <br>
+
+
+<input type="text"Name="date of birth">
+<br>
+
+
+<input type="radio" name="gender"value="Male" checked>Male<br>
+ <input type="radio"name="gender"value ="female ">female<br> 
+<input type="radio"name="gender"value="other"> other<br> 
+<input type="submit" value=submit>
+
+</form>
+
+</body>
+
+
+</html>
+
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "action_page.php".</p>
+
+
+</body
+
+>
